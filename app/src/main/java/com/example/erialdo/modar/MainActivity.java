38055ac947a70090b40f0682ee3463cd8d77cb04
Erialdo Meta <erialdo.meta@bashkia.local>
2018-11-16 10:13:41 +0100

    change imports

--- app/src/main/java/com/example/erialdo/modar/MainActivity.java
+++ app/src/main/java/com/example/erialdo/modar/MainActivity.java
@@ -1,23 +1,16 @@
 package com.example.erialdo.modar;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
 
 public class MainActivity extends Activity implements OnClickListener{
 
